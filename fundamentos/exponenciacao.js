9ee8473ba90c114492266ad87cf6c563e01916d1
lgustavs <lgustavs98@gmail.com>
2022-03-30 12:34:02 -0300

    Operadores: Exponenciação

---
+++ fundamentos/exponenciacao.js
@@ -0,0 +1,20 @@
+//Exponenciação maneira antiga
+let exp = Math.pow(2, 3) // O primeiro parâmetro é a base e o segundo o expoente
+
+console.log(exp)
+
+//ES7
+let exp2 = 2 ** 3
+
+console.log(exp)
+
+//Atribuição exponencial
+let base = 2 // Eleva o valor armazenado na base à terceira potência
+base **= 3
+
+console.log(base)
+
+//Expoente negativos
+let exp3 = 2 ** -3
+
+console.log(exp3)
